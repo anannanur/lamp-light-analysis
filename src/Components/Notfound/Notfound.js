721a6bba7827d6c25fd566b777f0306c1a408e3e
anannanur <anannaanu974@gmail.com>
2022-04-05 16:05:01 +0600

    404 page updated

--- src/Components/Notfound/Notfound.js
+++ src/Components/Notfound/Notfound.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import img from '../../images/notfound.gif';
 
 const Notfound = () => {
     return (
-        <div>
-            <h1>404</h1>
+        <div className='bg-dark'>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12 d-flex justify-content-center align-items-center">
+                        <img src={img} alt="" className='w-50 h-75'/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
